d59f81b014e02805d530f0dce629aa1a67c7da14
Tony Hunter <thunter1987@yahoo.com>
2023-07-24 17:45:04 -0500

    passes tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i=10
+    while i > 0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
+    
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_integers = [int*int for int in int_list]
+    return square_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range (1, 101):
+        if number % 5 == 0 and number % 3 == 0:
+            print ("FizzBuzz")
+        elif number % 5 == 0:
+            print("Buzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        else:
+            print(number)        
